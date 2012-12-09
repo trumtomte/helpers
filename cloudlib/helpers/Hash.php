b95b80483df5a1caf9f001a72926f81f98e3f766
Sebastian Book <sebbebook@gmail.com>
2012-12-09 21:03:36 +0100

    remade the Hash class so it is an object instead of only static methods

--- cloudlib/helpers/Hash.php
+++ cloudlib/helpers/Hash.php
@@ -25,7 +25,7 @@ class Hash
      * @access  public
      * @var     string
      */
-    public static $secret = null;
+    public $secret = null;
 
     /**
      * Constructor
@@ -33,7 +33,10 @@ class Hash
      * @access  public
      * @return  void
      */
-    public function __construct() {}
+    public function __construct($secret = null)
+    {
+        $this->secret = $secret;
+    }
 
     /**
      * Create a bcrypt hash
@@ -41,20 +44,19 @@ class Hash
      * @acess   public
      * @param   string  $value  The value to be hashed
      * @param   string  $salt   The hash salt
-     * @param   string  $secret The hash secret (static salt)
-     * @param   int     $rounds Number of rounds
-     * @return  string          Returns a hash
+     * @param   int     $cost   Cost of the hash
+     * @throws InvalidArgumentException If the cost is not between 4 and 31
+     * @return  string          Returns the hash string
      */
-    public static function create($value, $salt, $secret = null, $rounds = 12)
+    public function generate($value, $salt, $cost = 12)
     {
-        if($rounds > 31 || $rounds < 4)
+        if($cost > 31 || $cost < 4)
         {
-            throw new InvalidArgumentException('The number of rounds has to be between 4-31');
+            throw new InvalidArgumentException('The cost has to be between 4 and 31');
         }
 
-        $secret = static::$secret ? static::$secret : $secret;
-
-        $salt = sprintf('$2a$%02d$%s', $rounds, substr(base64_encode(sha1($salt . $secret)), 0, 22));
+        $salt = sprintf('$2a$%02d$%s', $cost,
+            substr(base64_encode(sha1($salt . $this->secret)), 0, 22));
 
         return substr(crypt($value, $salt), 7);
     }
@@ -66,14 +68,13 @@ class Hash
      * @param   string  $hash   The existing hash
      * @param   string  $value  The value to be hashed
      * @param   string  $salt   The hash salt
-     * @param   string  $secret The hash secret (static salt)
-     * @param   int     $rounds Number of rounds
-     * @return  boolean         Returns true if the comparison is equal
+     * @param   int     $cost   Cost of the hash
+     * @return  boolean         Returns true if it is the same hash
      */
-    public static function compare($hash, $value, $salt, $secret = null, $rounds = 12)
+    public function compare($hash, $value, $salt, $cost = 12)
     {
-        $secret = static::$secret ? static::$secret : $secret;
-
-        return (bool) (($new = static::create($value, $salt, $secret, $rounds)) == $hash);
+        $input = $this->generate($value, $salt, $cost);
+    
+        return (bool) ($input == $hash);
     }
 }
